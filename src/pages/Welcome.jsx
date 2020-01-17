3b1e772047b231a499a05fb28a311100d910f0a5
Miguel Diaz <51382978+mdiazcogent@users.noreply.github.com>
2020-01-17 15:14:43 -0600

    I deleted current home page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -15,40 +15,7 @@ const CodePreview = ({ children }) => (
 export default () => (
   <PageHeaderWrapper>
     <Card>
-      <Alert
-        message="umi ui 现已发布，点击右下角 umi 图标即可使用"
-        type="success"
-        showIcon
-        banner
-        style={{
-          margin: -12,
-          marginBottom: 24,
-        }}
-      />
-      <Typography.Text strong>
-        <a target="_blank" rel="noopener noreferrer" href="https://pro.ant.design/docs/block">
-          <FormattedMessage
-            id="app.welcome.link.block-list"
-            defaultMessage="基于 block 开发，快速构建标准页面"
-          />
-        </a>
-      </Typography.Text>
-      <CodePreview> npm run ui</CodePreview>
-      <Typography.Text
-        strong
-        style={{
-          marginBottom: 12,
-        }}
-      >
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://pro.ant.design/docs/available-script#npm-run-fetchblocks"
-        >
-          <FormattedMessage id="app.welcome.link.fetch-blocks" defaultMessage="获取全部区块" />
-        </a>
-      </Typography.Text>
-      <CodePreview> npm run fetch:blocks</CodePreview>
+      <div>Welcome to WMS</div>
     </Card>
     <p
       style={{
