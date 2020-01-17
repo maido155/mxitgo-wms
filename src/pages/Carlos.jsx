a5c915939f2f28ae0f37a55b42fffaf5428b2bd2
Miguel Diaz <51382978+mdiazcogent@users.noreply.github.com>
2020-01-17 15:25:11 -0600

    basic structure for course

---
+++ src/pages/Carlos.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+import { Card, Typography, Alert } from 'antd';
+import styles from './Carlos.less';
+
+const CodePreview = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default () => (
+  <PageHeaderWrapper>
+    <Card>
+      <div>Welcome to WMS</div>
+    </Card>
+    <p
+      style={{
+        textAlign: 'center',
+        marginTop: 24,
+      }}
+    >
+      Want to add more pages? Please refer to{' '}
+      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+        use block
+      </a>
+      。
+    </p>
+  </PageHeaderWrapper>
+);
