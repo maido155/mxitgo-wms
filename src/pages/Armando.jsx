f3ce8665d8e565f0a1231e16db416669a145cdb1
armando161210 <55868072+armando161210@users.noreply.github.com>
2020-01-20 21:52:02 -0600

    add components

--- src/pages/Armando.jsx
+++ src/pages/Armando.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { FormattedMessage } from 'umi-plugin-react/locale';
-import { Card, Typography, Select, notification, message  } from 'antd';
+import { Card, Typography, Select, notification, message, Row, Col, Button  } from 'antd';
 import styles from './Armando.less';
 
 const CodePreview = ({ children }) => (
@@ -14,6 +14,7 @@ const CodePreview = ({ children }) => (
 
 const {Option} = Select;
 const key = 'updatable';
+const {Meta} = Card;
 
 const openMessage = (value) => {
   message.loading({ content: 'Loading...', key });
@@ -29,6 +30,9 @@ function openNotification(value) {
       `Are you ${value}?`,
     onClick: () => {
       openMessage(value);
+      console.log("hello");
+      const numbre = 12;
+      console.log(numbre);
     }  
   });
 };
@@ -36,32 +40,52 @@ function openNotification(value) {
 export default () => (
   <PageHeaderWrapper>
     <Card>
-      <Select
-        showSearch
-        style={{ width: 200 }}
-        placeholder="Select a person"
-        optionFilterProp="children"
-        onChange={openNotification}
-      >
-        <Option value="armando">Armando</Option>
-        <Option value="hector">Hector</Option>
-        <Option value="carlos">Carlos</Option>
-        <Option value="miguel">Miguel</Option>
-        <Option value="diego">Diego</Option>
-      </Select>  
+      <div>
+        <Select
+          showSearch
+          style={{ width: 200 }}
+          placeholder="Select a person"
+          optionFilterProp="children"
+          onChange={openNotification}
+        >
+          <Option value="armando">Armando</Option>
+          <Option value="hector">Hector</Option>
+          <Option value="carlos">Carlos</Option>
+          <Option value="miguel">Miguel</Option>
+          <Option value="diego">Diego</Option>
+        </Select>
+      </div>
+
+      <br/>
+
+      <div>
+        <Row>
+          <Col span={12}>
+            <Card
+              hoverable
+              style={{ width: "100%" }}
+              cover={<img alt="example" src="https://www.superama.com.mx/Content/images/products/img_large/0750106410132L1.jpg" />}
+              >
+              <Meta title="Corona" description="https://cervezacorona.es" /> 
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card
+              hoverable
+              style={{ width: "100%" }}
+              cover={<img alt="example" src="https://www.superama.com.mx/Content/images/products/img_large/0750106419274L1.jpg" />}
+              >
+              <Meta title="Victoria" description="https://cervezavictoria.com.mx" /> 
+            </Card>
+          </Col>
+        </Row> 
+      </div> 
+
+      <br/>
+      
+      <div>
+        <Button type="primary">Primary</Button>  
+      </div> 
     </Card>
-    
-    <p
-      style={{
-        textAlign: 'center',
-        marginTop: 24,
-      }}
-    >
-      Want to add more pages? Please refer to{' '}
-      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-        use block
-      </a>
-      。
-    </p>
   </PageHeaderWrapper>
 );
