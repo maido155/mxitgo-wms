d4874431ef1275dcc03b9e507e08d4ed43ba6312
armando161210 <55868072+armando161210@users.noreply.github.com>
2020-03-03 17:36:28 -0600

    AccountSettings Put

--- src/pages/account/AccountSettings.jsx
+++ src/pages/account/AccountSettings.jsx
@@ -1,12 +1,14 @@
 import React, { PureComponent } from 'react';
 import { _ } from 'lodash'; 
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Card, Row, Col, Form, Input, Divider, Select } from 'antd';
+import { Card, Row, Col, Form, Input, Divider, Select, Spin, Button } from 'antd';
 import Upload from '../generalComponents/UploadAvatar';
 import Styles from './StylesAccount.css';
 import { connect } from 'dva';
 
 const { Option } = Select;
+var numberPhone = "";
+var ladaPhone = "+521";
 
 @connect(({ user, loading }) => ({
     user,
@@ -15,7 +17,10 @@ const { Option } = Select;
 }))
 
 class AccountSettings extends PureComponent{
-    
+    state = {
+        loading: false,
+    }
+
     componentDidMount() {
         this.props.dispatch({
            type: 'user/fetchUserByEmail',
@@ -27,6 +32,10 @@ class AccountSettings extends PureComponent{
        });
     }
 
+    handleSubmit = (info) =>{
+        console.log(info);
+    }
+
     render(){
 
         const formItemLayout = {
@@ -34,18 +43,18 @@ class AccountSettings extends PureComponent{
             wrapperCol: {xs: { span: 24 },sm: { span: 12 },md: { span: 14 },lg: { span: 14 },xl: { span: 14  }}
         };
 
-        const { form, loading, user: { user },userByEmail } = this.props;
+        const { form, loading, user: { user },userByEmail, submitting } = this.props;
         const { getFieldDecorator } = form;
     
-        
-          if(userByEmail.length > 0){
-            const number = userByEmail.phone_number;
-            const numberPhone = number.substr(4);
-            const ladaPhone = number.substr(-14,4);
-          }
+        const number = userByEmail.phone_number;
+
+        if(userByEmail.length != 0){
+            numberPhone = number.substr(4);
+            ladaPhone = number.substr(-14,4);
+        }
 
           const prefixSelector = getFieldDecorator('prefix', {
-            initialValue: "",
+            initialValue: ladaPhone,
           })(
             <Select onChange={this.handleChange}>
               <Option value="+521">+521</Option>
@@ -56,50 +65,78 @@ class AccountSettings extends PureComponent{
         return(
             <PageHeaderWrapper  title="Account Settings">
                 <Card>
-                    <Form {...formItemLayout}>
-                        <Row>
-                            <Col lg={12} xl={24} className={Styles.avatar}>
-                                <Upload/>
-                            </Col>
-                        </Row>
-                        <Divider/>
-                        <Row>
-                            <Col lg={12} xl={12}>
-                                <Form.Item label="Nombre(s):">
-                                    {getFieldDecorator('name', { initialValue: userByEmail.name,
-                                        rules: [{ required: true, message: 'Ingresa tu nombre!'}]})
-                                    (<Input  defaultValue="hola" />)}
-                                </Form.Item>
-                            </Col>
-                            <Col lg={12} xl={12}>
-                                <Form.Item label="Apellido Paterno:">
-                                    {getFieldDecorator('family_name',{ initialValue: userByEmail.family_name,
-                                        rules: [{ required: true, message: 'Ingresa tu apellido paterno!'}]})
-                                    (<Input/>)}
-                                </Form.Item>
-                            </Col>
-                            <Col lg={12} xl={12}>
-                                <Form.Item label="Apellido Materno:">
-                                    {getFieldDecorator('middle_name',{ initialValue: userByEmail.middle_name,
-                                        rules: [{ required: true, message: 'Ingresa tu apellido materno!'}]})
-                                    (<Input/>)}
-                                </Form.Item>
-                            </Col>
-                            <Col lg={12} xl={12}>
-                                <Form.Item label="Telefono:">
-                                    {getFieldDecorator('phone_number', { initialValue: "",
-                                        rules: [{ required: true, message: 'Por favor ingrese su número de teléfono!' },
-                                        { pattern: /^\d{10}$/, message: 'Formato invalido!'}],})
-                                        (<Input addonBefore={prefixSelector}/>)}
-                                </Form.Item>
-                            </Col>
-                            <Col lg={12} xl={12}>
-                                <Form.Item label="Correo Electronico:">
-                                    <Input disabled defaultValue={userByEmail.email}/>
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                    </Form>
+                    <Spin tip="Loading Account" spinning={loading}>
+                        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                            <Row>
+                                <Col lg={12} xl={24} className={Styles.avatar}>
+                                    <Upload/>
+                                </Col>
+                            </Row>
+                            <Divider/>
+                            <Row>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item label="Nombre(s):">
+                                        {getFieldDecorator('name', { initialValue: userByEmail.name,
+                                            rules: [{ required: true, message: 'Ingresa tu nombre!'}]})
+                                        (<Input  defaultValue="hola" />)}
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item label="Apellido Paterno:">
+                                        {getFieldDecorator('family_name',{ initialValue: userByEmail.family_name,
+                                            rules: [{ required: true, message: 'Ingresa tu apellido paterno!'}]})
+                                        (<Input/>)}
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item label="Apellido Materno:">
+                                        {getFieldDecorator('middle_name',{ initialValue: userByEmail.middle_name,
+                                            rules: [{ required: true, message: 'Ingresa tu apellido materno!'}]})
+                                        (<Input/>)}
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item label="Telefono:">
+                                        {getFieldDecorator('phone_number', { initialValue: numberPhone,
+                                            rules: [{ required: true, message: 'Por favor ingrese su número de teléfono!' },
+                                            { pattern: /^\d{10}$/, message: 'Formato invalido!'}],})
+                                            (<Input addonBefore={prefixSelector}/>)}
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item label="Correo Electronico:">
+                                        {getFieldDecorator('email',{ initialValue: userByEmail.email, 
+                                            rules: [{ required: true, message: 'Ingresa tu correo electronico!'},{ type: 'email', message: 'Este correo no es valido!'}]})
+                                            (<Input disabled/>)}
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={12} xl={12}>
+                                    <Form.Item>
+                                        <Button
+                                            size="large"
+                                            loading={submitting}
+                                            type="primary"
+                                            htmlType="submit"
+                                        >
+                                            Guardar
+                                        </Button>  
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            {/* <Row>
+                                <Col span={22} className={Styles.botton}>
+                                    <Button
+                                        size="large"
+                                        loading={submitting}
+                                        type="primary"
+                                        htmlType="submit"
+                                    >
+                                        Guardar
+                                    </Button>     
+                                </Col>
+                            </Row> */}
+                        </Form>
+                    </Spin>
                 </Card>
         </PageHeaderWrapper>
         );
