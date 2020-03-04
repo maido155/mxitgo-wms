caf2e96597f7547cfe355949b15cd51c2887524d
HectorRobl3do <38726176+HectorRobl3do@users.noreply.github.com>
2020-03-03 22:09:56 -0600

    General programming modal Edit

--- src/pages/generalProgramming/ModalEdit.jsx
+++ src/pages/generalProgramming/ModalEdit.jsx
@@ -8,44 +8,24 @@ import EditData from './EditData';
 
 
 class ModalEdit extends PureComponent {
-  state = { visible: false };
+ 
 
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
+  
 
   render() {
     return (
       <div>
-        <Button type="primary" shape="circle" icon="plus" onClick={this.showModal}>
-        </Button>
         <Modal
           title="Nueva programación"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
+          visible={this.props.stateModalEdit}
+          onOk={this.props.ok}
+          onCancel={this.props.cancel}
           
           footer={[
-            <Button key="Limpiar" onClick={this.handleCancel}>
+            <Button key="Limpiar" onClick={this.props.cancel}>
               Limpiar
             </Button>,
-            <Button key="Aceptar" type="primary" onClick={this.handleOk}>
+            <Button key="Aceptar" type="primary" onClick={this.props.ok}>
               Aceptar
             </Button>,
           ]}
--- src/pages/generalProgramming/tableGeneralProgramming.jsx
+++ src/pages/generalProgramming/tableGeneralProgramming.jsx
@@ -2,128 +2,163 @@ import React, { PureComponent } from 'react';
 import { _ } from 'lodash'; 
 import {isMobile} from 'react-device-detect';
 import { Table, Divider, Icon } from 'antd';
+import ModalEdit from './ModalEdit'
 
-const columns = [
-  {
-    title: 'Semana',
-    dataIndex: 'week',
-
-  },
-  {
-    title: 'Tipo de producto',
-    dataIndex: 'products',
-  },
-  {
-    title: 'Cliente',
-    dataIndex: 'client',
-  },
-  {
-      title: 'Cajas',
-      dataIndex: 'box',
-    },
-    {
-      title: 'Pallets',
-      dataIndex: 'pallets',
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-    }
-];
-
-const data = [
-  {
-    key: '1',
-    week: '10 - 12',
-    products: "Gold",
-    client: 'Vallejo',
-    box:  "7400",
-    pallets: "500",
-    status: "Programado",
-   
-  },
-  {
-      key: '2',
-    week: '6-12',
-    products: "Premium",
-    client: 'Cuautitlán',
-    box:  "8200",
-    pallets: "530",
-    status: "220",
-   
-  },
-  {
-      key: '3',
-    week: '31-5',
-    products: "Gold",
-    client: 'Reparto',
-    box:  "7400",
-    pallets: "500",
-    status: "43",
-   
-  },
-];
-
-  if(isMobile){
-    columns.push(
-      {
-        title: 'Acciones',
-        key: 'action',
-        fixed: 'right',
-       
-        render: () => (
-          <span>
-           <a> <Icon type="edit" theme="filled" /> </a> 
-            <Divider type="vertical" />
-           <a><Icon type="close-circle" theme="filled" /></a> 
-            <Divider type="vertical" />
-            <a>  <Icon type="arrows-alt" /></a> 
-            <Divider type="vertical" />
-            <a>  <Icon type="eye" theme="filled"  /> </a> 
-          </span>
-        ),
-      }
-    );
-  }else{
-    columns.push(
-      {
-        title: 'Acciones',
-        key: 'action',
-        fixed: 'right',
-       
-        render: () => (
-          <span>
-            <a><Icon type="edit" theme="filled" /></a>
-            <a> Editar</a>
-            <Divider type="vertical" />
-            <a><Icon type="close-circle" theme="filled" /></a>
-            <a> Cancelar</a>
-            <Divider type="vertical" />
-            <a><Icon type="arrows-alt" /></a>
-            <a> Pendiente</a>
-            <Divider type="vertical" />
-            <a><Icon type="eye" theme="filled"  /></a>
-            <a> Visualizar</a>
-          </span>
-        ),
-      }
-    );
-  }
-
-const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    getCheckboxProps: record => ({
-      disabled: record.name === 'Disabled User',
-      name: record.name,
-    }),
-}; 
 
 class TableGeneralProgramming extends PureComponent{
+state = { visibleEdit: false };
+
+showModalEdit = () => {
+  this.setState({
+    visibleEdit: true,
+  });
+};
+
+handleOkEdit = e => {
+  console.log(e);
+  this.setState({
+    visibleEdit: false,
+  });
+};
+
+handleCancelEdit = e => {
+  console.log(e);
+  this.setState({
+    visibleEdit: false,
+  });
+};
+
+
+//pego las funciones bajo el class
+
     render(){
+    
+      const columns = [
+        {
+          title: 'Semana',
+          dataIndex: 'week',
+      
+        },
+        {
+          title: 'Tipo de producto',
+          dataIndex: 'products',
+        },
+        {
+          title: 'Cliente',
+          dataIndex: 'client',
+        },
+        {
+            title: 'Cajas',
+            dataIndex: 'box',
+          },
+          {
+            title: 'Pallets',
+            dataIndex: 'pallets',
+          },
+          {
+            title: 'Status',
+            dataIndex: 'status',
+          }
+      ];
+      
+      const data = [
+        {
+          key: '1',
+          week: '10 - 12',
+          products: "Gold",
+          client: 'Vallejo',
+          box:  "7400",
+          pallets: "500",
+          status: "Programado",
+         
+        },
+        {
+            key: '2',
+          week: '6-12',
+          products: "Premium",
+          client: 'Cuautitlán',
+          box:  "8200",
+          pallets: "530",
+          status: "220",
+         
+        },
+        {
+            key: '3',
+          week: '31-5',
+          products: "Gold",
+          client: 'Reparto',
+          box:  "7400",
+          pallets: "500",
+          status: "43",
+         
+        },
+      ];
+      
+        if(isMobile){
+          columns.push(
+            {
+              title: 'Acciones',
+              key: 'action',
+              fixed: 'right',
+             
+              render: () => (
+                <span>
+                 <a> <Icon type="edit" theme="filled" /> </a> 
+                  <Divider type="vertical" />
+                 <a onClick={this.showModalEdit}><Icon type="close-circle" theme="filled" /></a> 
+                  <Divider type="vertical" />
+                  <a>  <Icon type="arrows-alt" /></a> 
+                  <Divider type="vertical" />
+                  <a>  <Icon type="eye" theme="filled"  /> </a> 
+                </span>
+              ),
+            }
+          );
+        }else{
+          columns.push(
+            {
+              title: 'Acciones',
+              key: 'action',
+              fixed: 'right',
+             
+              render: () => (
+                <span>
+                  <a><Icon type="edit" theme="filled" /></a>
+                  <a onClick={this.showModalEdit}> Editar</a> 
+                  <Divider type="vertical" />
+                  <a><Icon type="close-circle" theme="filled" /></a>
+                  <a> Cancelar</a>
+                  <Divider type="vertical" />
+                  <a><Icon type="arrows-alt" /></a>
+                  <a> Pendiente</a>
+                  <Divider type="vertical" />
+                  <a><Icon type="eye" theme="filled"  /></a>
+                  <a> Visualizar</a>
+                </span>
+              ),
+            }
+          );
+        }
+      
+      const rowSelection = {
+          onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+          },
+          getCheckboxProps: record => ({
+            disabled: record.name === 'Disabled User',
+            name: record.name,
+          }),
+      }; 
+      
         return(
-            <Table  style={{marginTop: "2%"}} size="small" rowSelection={rowSelection} columns={columns} dataSource={data}  />
+           <div>
+            <ModalEdit
+            stateModalEdit = {this.state.visibleEdit}
+            ok = {this.handleOkEdit}
+            cancel = {this.handleCancelEdit}
+            />
+              <Table  style={{marginTop: "2%"}} size="small" rowSelection={rowSelection} columns={columns} dataSource={data}  />
+           </div>
         );
     }
 }
