d0ef03e8fe7a8d421705e2d5befd9db5a0b3f55e
armando161210 <55868072+armando161210@users.noreply.github.com>
2020-03-24 22:43:19 -0600

    fix entry

--- src/pages/shipping/gridModalEntry.jsx
+++ src/pages/shipping/gridModalEntry.jsx
@@ -12,52 +12,59 @@ class gridModalEntry extends PureComponent {
         return (
             <div>
                 <Row type="flex" justify="space-around">
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
-                        <Text strong>Cantidades:</Text>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3}></Col>
+                    <Col xs={24} sm={5} md={4} lg={3} xl={2} className={Styles.labelTitle}>
+                        <Text strong>Cantidades</Text>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={7} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
                         <Text>Premium</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={5} sm={3} md={3} lg={3} xl={3} className={Styles.label}>
                         <Text>Gold</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={7} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
                         <Text>Segunda</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={5} sm={3} md={3} lg={3} xl={3} className={Styles.label}>
                         <Text>Mano</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={5} sm={3} md={3} lg={3} xl={3} className={Styles.label}>
                         <Text>Dedo</Text>
                         <Input/>
                     </Col>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3}></Col>
                 </Row>
-
-                <Row type="flex" justify="space-around" className={Styles.input}>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
-                        <Text></Text>
-                        <Text strong>Temperatura:</Text>
+                {/* <Row type="flex" justify="space-around" className={Styles.row}>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3}></Col>
+                    <Col xs={24} sm={24} md={4} lg={3} xl={2} className={Styles.label}>
+                        <Text strong>Temperatura</Text>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={7} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
+                        <Text>Premium</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={7} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
+                        <Text>Gold</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={7} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
+                        <Text>Segunda</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={11} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
+                        <Text>Mano</Text>
                         <Input/>
                     </Col>
-                    <Col xs={8} sm={7} md={6} lg={6} xl={3} className={Styles.label}>
+                    <Col xs={11} sm={4} md={3} lg={3} xl={3} className={Styles.label}>
+                        <Text>Dedo</Text>
                         <Input/>
                     </Col>
-                </Row>
+                    <Col xs={0} sm={0} md={1} lg={2} xl={3}></Col>
+                </Row> */}
             </div>
         );
     }
--- src/pages/shipping/modalEntry.jsx
+++ src/pages/shipping/modalEntry.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { _ } from 'lodash';
-import {Button,Divider,Modal,Icon,Form, Row,Col} from 'antd';
+import {Button,Divider,Modal,Icon,Form, Row,Col, Radio }from 'antd';
 import TableModal from './tableModalEntry';
 import RadioGroupComponent from '../generalComponents/RadioGroupComponent';
 import { formatMessage } from 'umi-plugin-react/locale';
@@ -8,10 +8,6 @@ import GridModal from './gridModalEntry';
 
 class ModalEntry extends PureComponent {
   render() {
-    const formItemLayout = {
-      labelCol: {xs: { span: 24 },sm: { span: 9 },md: { span: 9 },lg: { span: 9 },xl: { span: 9 }},
-      wrapperCol: {xs: { span: 24 },sm: { span: 15 },md: { span: 15 },lg: { span: 15 },xl: { span: 15  }}
-    };
     return (
       <Modal
         width= '80%'
@@ -30,11 +26,14 @@ class ModalEntry extends PureComponent {
         ]}
       >
         <TableModal/>
-        <Form {...formItemLayout} style={{marginTop: "1rem"}}>
+        <Form style={{marginTop: "1rem"}}>
           <Row type="flex" justify="center">
-            <Col xs={24} sm={16} md={16} lg={16} xl={16}>
-              <Form.Item label={formatMessage({ id: 'outWard.label.product' })}>
-                <RadioGroupComponent/>
+            <Col xs={20} sm={10} md={7} lg={6} xl={4}>
+              <Form.Item>
+                <Radio.Group defaultValue="a">
+                  <Radio.Button value="a">Recibidos</Radio.Button>
+                  <Radio.Button value="b">Merma</Radio.Button>
+                </Radio.Group>
               </Form.Item>
             </Col>
           </Row>
