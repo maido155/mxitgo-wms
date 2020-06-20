13e3cf96e349ebc66b8631172308c5216528e01f
armando161210 <55868072+armando161210@users.noreply.github.com>
2020-06-19 19:26:36 -0500

    I changed the width of the drawer

--- src/pages/generalProgramming/drawerGeneralProgramming.jsx
+++ src/pages/generalProgramming/drawerGeneralProgramming.jsx
@@ -38,29 +38,28 @@ class DrawerGeneralProgramming extends PureComponent {
         return(
             <Drawer
                 title={<FormattedMessage id="general.modal-title.title"/>}
-                width={isMobile ? "100%" : "50%"}
+                width={isMobile ? "100%" : "30%"}
                 closable={true}
                 onClose={this.props.onCloseNewDrawer}
                 visible={this.props.visibleNewDrawer}
             >
                 <Form {...formItemLayout}>
                     <Row>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
-                        <Col xs={5} sm={6} md={6} lg={7} xl={8}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label={formatMessage({id: "general.calendar.week"})}/>
                         </Col>
-                        <Col xs={19} sm={18} md={18} lg={17} xl={10}>
+                        <Col xs={16} sm={24} md={24} lg={16} xl={14}>
                             <RangePicker style={{ width: '100%'}}/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
                     </Row>
                     <Divider/>
                     <Row>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
-                        <Col xs={5} sm={6} md={6} lg={7} xl={8}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label={formatMessage({id: "general.buttoon-product.product"})}/>
                         </Col>
-                        <Col xs={19} sm={18} md={18} lg={17} xl={10}>
+                        <Col xs={16} sm={24} md={24} lg={16} xl={14}>
                             <Select
                                 showSearch
                                 style={{ width: 200 }}
@@ -78,14 +77,13 @@ class DrawerGeneralProgramming extends PureComponent {
                                 <Option value="premium">Premium</Option>
                             </Select>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
                     </Row>
                     <Row>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
-                        <Col xs={5} sm={6} md={6} lg={7} xl={8}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label={formatMessage({id: "general.buttoon-center.center"})}/>
                         </Col>
-                        <Col xs={19} sm={18} md={18} lg={17} xl={10}>
+                        <Col xs={16} sm={24} md={24} lg={16} xl={14}>
                             <Select
                                 showSearch
                                 style={{ width: 200 }}
@@ -108,121 +106,109 @@ class DrawerGeneralProgramming extends PureComponent {
                     </Row>
                     <Divider/>
                     <Row>
-                        <Col xs={8} sm={9} md={9} lg={10} xl={9}></Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={0} md={0} lg={8} xl={9}></Col>
+                        <Col xs={8} sm={0} md={0} lg={8} xl={7} style={{paddingBottom: ".7rem"}}>
                             <label style={ isMobile ? { fontSize: "1rem", marginLeft: "25%"} : { fontSize: "1rem", marginLeft: "20%"}}><FormattedMessage id="general.modal-label.pallets"/></label>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
-                        <Col xs={8} sm={7} md={7} lg={7} xl={4}>
+                        <Col xs={8} sm={0} md={0} lg={8} xl={7} style={{paddingBottom: ".7rem"}}>
                             <label style={isMobile ? { fontSize: "1rem", marginLeft: "25%"} : { fontSize: "1rem", marginLeft: "20%"}}><FormattedMessage id="general.modal-label.boxes"/></label>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={0}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Jue 8 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Vie 9 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Sab 10 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Dom 11 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Lun 12 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
-
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Mar 13 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={5} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={5} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Mie 14 Nov"/>
                         </Col>
-                        <Col xs={8} sm={8} md={8} lg={6} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={7} sm={7} md={7} lg={5} xl={4}>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7}>
                             <InputNumber/>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={5} xl={6}></Col>
                     </Row>
                     <Row>
-                        <Col xs={1} sm={0} md={0} lg={0} xl={6}></Col>
-                        <Col xs={7} sm={9} md={9} lg={9} xl={3}>
+                        <Col xs={1} sm={0} md={0} lg={0} xl={2}></Col>
+                        <Col xs={7} sm={17} md={17} lg={8} xl={7}>
                             <Form.Item label="Total"/>
                         </Col>
-                        <Col xs={5} sm={5} md={5} lg={5} xl={2}>
-                            <label style={{ marginLeft:"50%"}}>0</label>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7} style={{paddingTop: ".5rem"}}>
+                            <label style={{ marginLeft:"35%"}}>0</label>
                         </Col>
-                        <Col xs={3} sm={3} md={3} lg={3} xl={2}></Col>
-                        <Col xs={5} sm={5} md={5} lg={3} xl={2}>
-                            <label style={{ marginLeft:"50%"}}>0</label>
+                        <Col xs={8} sm={12} md={12} lg={8} xl={7} style={{paddingTop: ".5rem"}}>
+                            <label style={{ marginLeft:"35%"}}>0</label>
                         </Col>
-                        <Col xs={0} sm={0} md={0} lg={0} xl={6}></Col>
                     </Row>
                 </Form>
                 <div
